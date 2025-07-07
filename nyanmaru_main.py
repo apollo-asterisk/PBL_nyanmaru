4150f078e7ed5565e03207e97ab00598ff4f6753
satokannna <138266897+apollo-asterisk@users.noreply.github.com>
2025-07-07 10:34:31 +0900

    Update nyanmaru_main.py

--- nyanmaru_main.py
+++ nyanmaru_main.py
@@ -21,17 +21,33 @@ SERVO_PIN_1 = 13
 SERVO_PIN_2 = 12
 pi = pigpio.pi()
 
+BUTTON_PIN=31
+
 GPIO.setup(AIN1, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(AIN2, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(PWMA, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(BIN1, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(BIN2, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(PWMB, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(BUTTON_PIN,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-p_a = GPIO.PWM(PWMA, 100)
-p_b = GPIO.PWM(PWMB, 100)
+p_a = GPIO.PWM(PWMA, 50)
+p_b = GPIO.PWM(PWMB, 50)
 p_a.start(100)
 p_b.start(100)
+
+running =False
+
+def button_callback(channel):
+    global running
+    running =not running
+    if running:
+        print("start")
+    else:
+        print("stop")
+        func_stop()
+        
+GPIO.add_event_detect(BUTTON_PIN,GPIO.FALLING,callback=button_callback,bouncetime=200)
 
 def func_forward():
     GPIO.output(AIN1, GPIO.HIGH)
@@ -53,11 +69,23 @@ def func_stop():
 
 def move_ears():
     # サーボを動かす（例：2.4ms→0.5ms→中央に戻す）
-    pi.hardware_PWM(SERVO_PIN_1, 50, int(1000000*2.4/20))
-    pi.hardware_PWM(SERVO_PIN_2, 50, int(1000000*2.4/20))
+    pi.hardware_PWM(SERVO_PIN_1, 50, int(1000000*1.4/20))
+    pi.hardware_PWM(SERVO_PIN_2, 50, int(1000000*0.5/20))
     time.sleep(0.5)
     pi.hardware_PWM(SERVO_PIN_1, 50, int(1000000*0.5/20))
+    pi.hardware_PWM(SERVO_PIN_2, 50, int(1000000*1.4/20))
+    time.sleep(0.5)
+    pi.hardware_PWM(SERVO_PIN_1, 50, int(1000000*1.4/20))
     pi.hardware_PWM(SERVO_PIN_2, 50, int(1000000*0.5/20))
+    time.sleep(0.5)
+    pi.hardware_PWM(SERVO_PIN_1, 50, int(1000000*0.5/20))
+    pi.hardware_PWM(SERVO_PIN_2, 50, int(1000000*1.4/20))
+    time.sleep(0.5)
+    pi.hardware_PWM(SERVO_PIN_1, 50, int(1000000*1.4/20))
+    pi.hardware_PWM(SERVO_PIN_2, 50, int(1000000*0.5/20))
+    time.sleep(0.5)
+    pi.hardware_PWM(SERVO_PIN_1, 50, int(1000000*0.5/20))
+    pi.hardware_PWM(SERVO_PIN_2, 50, int(1000000*1.4/20))
     time.sleep(0.5)
     # サーボを停止
     pi.hardware_PWM(SERVO_PIN_1, 50, 0)
@@ -107,60 +135,66 @@ picam2.start()
 
 try:
     motor_state = None  # "right"または"forward"または"stop"
-    stopped_for_face = False
+#     stopped_for_face = False
     while True:
-        frame = picam2.capture_array()
-        img = cv2.flip(frame, 1)
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        if running:
+            frame = picam2.capture_array()
+            img = cv2.flip(frame, 1)
+            gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.2,
-            minNeighbors=5,
-            minSize=(int(minW), int(minH)),
-        )
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.2,
+                minNeighbors=5,
+                minSize=(int(minW), int(minH)),
+            )
 
-        if len(faces) > 0:
-            # 顔が見つかった
-            distance = get_distance()
-            print(f"Distance: {distance:.1f}cm")
-            if 0 < distance < 10:
-                if motor_state != "stop":
-                    func_stop()
-                    print("10cm未満なので停止し耳を動かします")
-                    move_ears()
-                    motor_state = "stop"
-                    stopped_for_face = True
+            if len(faces) > 0:
+                # 顔が見つかった
+                distance = get_distance()
+                print(f"Distance: {distance:.1f}cm")
+                if 0 < distance < 25:
+                    if motor_state != "stop":
+                        func_stop()
+                        print("25cm未満なので停止し耳を動かします")
+                        motor_state = "stop"
+                        move_ears()
+    #                     stopped_for_face = True
+                else:
+                    if motor_state != "forward":
+                        func_forward()
+                        print("前進")
+                        motor_state = "forward"
+    #                 stopped_for_face = False
             else:
-                if motor_state != "forward":
-                    func_forward()
-                    print("前進")
-                    motor_state = "forward"
-                stopped_for_face = False
-        else:
-            # 顔がなければ回転
-            if motor_state != "right":
-                func_right()
-                print("顔が見つからないので回転")
-                motor_state = "right"
-            stopped_for_face = False
+                # 顔がなければ回転
+                if motor_state != "right":
+                    func_right()
+                    print("顔が見つからないので回転")
+                    motor_state = "right"
+    #             stopped_for_face = False
 
         # 画面表示（オプション）
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-            if confidence < 100:
-                label = names[id]
-                conf_str = "  {0}%".format(round(100 - confidence))
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+                if confidence < 100:
+                    label = names[id]
+                    conf_str = "  {0}%".format(round(100 - confidence))
+                else:
+                    label = "unknown"
+                    conf_str = "  {0}%".format(round(100 - confidence))
+                cv2.putText(img, str(label), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+                cv2.putText(img, str(conf_str), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+                cv2.imshow('camera', img)
+            
             else:
-                label = "unknown"
-                conf_str = "  {0}%".format(round(100 - confidence))
-            cv2.putText(img, str(label), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
-            cv2.putText(img, str(conf_str), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
-        cv2.imshow('camera', img)
+                if moter_state !="stop":
+                    func_stop()
+                    moter_state="stop"
 
-        if cv2.waitKey(10) & 0xff == 27:  # ESCで終了
-            break
+            if cv2.waitKey(10) & 0xff == 27:  # ESCで終了
+                break
 
 finally:
     p_a.stop()
